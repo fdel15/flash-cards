814225fee7817a4cf17c5b1460d50965f5ebc8fe
fdel15 <frankdelpidio@gmail.com>
2017-01-11 08:39:28 -0500

    define category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+
+  get '/new_category', to: 'flash_cards#new_category'
+  post '/new_category', to: 'flash_cards#create_category', as: 'create_category'
 end

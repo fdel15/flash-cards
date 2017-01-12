c381ddd449dee39c74eebd7bf885ffd644847cbe
fdel15 <frankdelpidio@gmail.com>
2017-01-12 08:11:25 -0500

    make description and category_id not nullable

--- db/migrate/20170110112222_create_cards.rb
+++ db/migrate/20170110112222_create_cards.rb
@@ -1,8 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.belongs_to :category
-      t.string :description
+      t.belongs_to :category, null: false
+      t.string :description, null: false
       t.boolean :front
       t.boolean :back
       t.integer :other_side_of_card_fk, index: true

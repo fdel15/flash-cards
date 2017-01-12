b1962110b96f4b4056e76f7b3776045f44192f3b
fdel15 <frankdelpidio@gmail.com>
2017-01-12 08:09:39 -0500

    add new card routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get '/new_category', to: 'flash_cards#new_category'
   post '/new_category', to: 'flash_cards#create_category', as: 'create_category'
+
+  get '/new_card', to: 'flash_cards#new_card'
+  post '/new_card', to: 'flash_cards#create_card', as: 'create_card'
+  
 end

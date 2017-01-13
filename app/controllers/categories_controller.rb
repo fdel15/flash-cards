e68a75876193c26954f4d100d70723ac46774ee0
Frank DelPidio <frankdaedalus@gmail.com>
2017-01-12 19:57:45 -0500

    add cateogry_params method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,4 +13,11 @@ class CategoriesController < ApplicationController
     end
     redirect_to root_path
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:description)
+  end
+
 end

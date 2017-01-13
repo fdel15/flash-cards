dc1aa53b01b5c38001b9e3a1c7cf6335952b731b
Frank DelPidio <frankdaedalus@gmail.com>
2017-01-12 19:51:03 -0500

    replace category routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
-  get '/new_category', to: 'flash_cards#new_category'
-  post '/new_category', to: 'flash_cards#create_category', as: 'create_category'
+resources :categories
 
   get '/new_card', to: 'flash_cards#new_card'
   post '/new_card', to: 'flash_cards#create_card', as: 'create_card'
-  
+
 end

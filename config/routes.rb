d88778d5e7fd7dd47c324a7ad70f7189f7e8f4b4
Frank DelPidio <frankdaedalus@gmail.com>
2017-01-12 20:03:09 -0500

    replace card routes with cards resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'static_pages#index'
+  resources :categories
+  resources :cards
 
-resources :categories
-
-  get '/new_card', to: 'flash_cards#new_card'
-  post '/new_card', to: 'flash_cards#create_card', as: 'create_card'
 
 end

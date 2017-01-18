f47300255d680af8297f703ea3447c05209c94cd
fdel15 <frankdelpidio@gmail.com>
2017-01-18 09:00:41 -0500

    create edit and update actions

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,8 +11,8 @@ class CardsController < ApplicationController
   end
 
   def create
-    front  = Card.new(description: card_params[:front], category_id: card_params[:category], front: 1)
-    back   = Card.new(description: card_params[:back], category_id: card_params[:category], back: 1)
+    front  = Card.new(description: card_params[:front], category_id: card_params[:category_id], front: 1)
+    back   = Card.new(description: card_params[:back], category_id: card_params[:category_id], back: 1)
     if front.valid? && back.valid?
       front.save
       back.flip_side_id = front.id
@@ -23,6 +23,25 @@ class CardsController < ApplicationController
       flash[:error] = "Oops something went wrong! Try again"
       redirect_to :back
     end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+    @categories = Category.all
+  end
+
+  def update
+    card = Card.find(params[:id])
+    if card.update(card_params)
+      card.keep_flip_category_in_sync
+      flash[:success] = "Card Updated!"
+    else
+      flash[:error] = "Oops something went wrong! Card not updated"
+    end
+    redirect_to cards_path
+  end
+
+  def delete
   end
 
   def review
@@ -39,7 +58,7 @@ class CardsController < ApplicationController
   private
 
   def card_params
-    params.require(:card).permit(:category, :front, :back )
+    params.require(:card).permit(:category_id, :front, :back, :id, :description )
   end
 
 end

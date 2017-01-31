a5199530d843738a64ae72b3c09d63bafa235198
Frank DelPidio <frankdaedalus@gmail.com>
2017-01-31 11:03:40 -0500

    create update_streak method

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,11 +5,16 @@ class Card < ApplicationRecord
   validates :description, presence: true, length: { minimum: 1 }
   validates :category_id, presence: true
 
-  def flip_side_description
-    Card.find(self.flip_side_id).description
+  def flip_side
+    Card.find(self.flip_side_id)
   end
 
   def keep_flip_category_in_sync
     Card.find(self.flip_side_id).update(category_id: self.category_id)
+  end
+
+  def update_streak(correct)
+    correct == "true" ? self.streak += 1 : self.streak = 0
+    self.save
   end
 end
